b9da23bb59ddfbcfe5d3408c44cf902373aa326d
Marcin Tolysz <tolysz@gmail.com>
2015-05-18 02:43:15 +0100

    Fix dependencies wave

--- setSandbox.sh
+++ setSandbox.sh
@@ -3,7 +3,6 @@
 rm -rf .cabal-sandbox
 cabal sandbox init
 
-cabal sandbox add-source /home/m/workHS/github/aeson
 #cabal sandbox add-source /home/m/workHS/github/Haxl
 cabal sandbox add-source /home/m/workHS/github/google-api
 cabal sandbox add-source /home/m/workHS/github/typedquery
@@ -12,10 +11,12 @@ cabal sandbox add-source /home/m/workHS/github/http-client-extra
 cabal sandbox add-source /home/m/workHS/github/http-client-extra-oauth2
 cabal sandbox add-source /home/m/workHS/github/yesod-websockets-extra
 cabal sandbox add-source /home/m/workHS/github/yesod-angular-ui
+cabal sandbox add-source /home/m/workHS/github/aeson
+cabal sandbox add-source /home/m/workHS/github/fb
+cabal sandbox add-source /home/m/workHS/github/keys
 
+#cabal install -j typedquery -fdebug-typed-queries
 
-cabal install -j typedquery -fdebug-typed-queries
+cabal install -j --constraint="typedquery +debug-typed-queries" -fdebug-typed-queries
 
-cabal install -j --dependencies-only --constraint="typedquery +debug-typed-queries" -fdebug-typed-queries 
-
-cabal build 
+#cabal build
